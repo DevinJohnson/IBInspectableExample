b053ee4856cffe9b3ffe8b17830920d255a95923
Devin Johnson <djohnson@grio.com>
2016-04-07 11:07:44 -0700

    Initial Commit

---
+++ IBTechTalk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IBTechTalk
+//
+//  Created by Devin Johnson on 4/7/16.
+//  Copyright © 2016 Devin Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IBTechTalk/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IBTechTalk
+//
+//  Created by Devin Johnson on 4/7/16.
+//  Copyright © 2016 Devin Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
